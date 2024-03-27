821527ae45d0f346f8d312db5ea35bdf8932160e
akurok <vladkonovalov20060@gmail.com>
2024-03-27 20:54:30 +0500

    Добавил преколлов

--- CtypyoApp/CryptoCurrency.cs
+++ CtypyoApp/CryptoCurrency.cs
@@ -6,9 +6,8 @@ public class CryptoCurrency
     public string Id { get; set; }
     public string Image { get; set; }
     public string Name { get; set; }
-    public double Volume { get; set; }
-    public double MaxPrice24h { get; set; }
-    public double MinPrice24h { get; set; }
+    [JsonProperty("total_volume")]
+    public decimal Volume { get; set; }
     [JsonProperty("current_price")]
     public decimal Price { get; set; }
     public override string ToString()
