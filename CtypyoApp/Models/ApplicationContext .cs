48ce6c101f849f7f23c9cb9db68665c57e356c89
akurok <vladkonovalov20060@gmail.com>
2024-03-30 18:14:29 +0500

    Добавил бд sqlLite и чуть подправил

---
+++ CtypyoApp/Models/ApplicationContext .cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtypyoApp.Models
+{
+    class ApplicationContext:DbContext
+    {
+        public DbSet<CryptoCurrency> CryptoCoin { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=coin.db");
+        }
+
+    }
+}
